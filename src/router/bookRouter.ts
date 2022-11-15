57003cdc4287a4870e3809db656752a3ddd5b31b
namjiyun <namjiyun3275@gmail.com>
2022-11-15 11:21:42 +0900

    [MERGE]

--- src/router/bookRouter.ts
+++ src/router/bookRouter.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/router/bookRouter.ts
 import { Router } from 'express';
 import { bookController } from '../controller';
 import { body } from 'express-validator';
@@ -6,19 +5,5 @@ const { validatorErrorChecker } = require('../middlewares/validator');
 const router: Router = Router();
 
 router.get('/', bookController.getBookList);
-=======
-// import { Router } from 'express';
-// import { userController } from '../controller';
-// import { body } from 'express-validator';
-// const { validatorErrorChecker } = require('../middlewares/validator');
-// const router: Router = Router();
 
-// router.get('/:userId', userController.getUserById);
-
-// router.post('/', body('name').notEmpty(), validatorErrorChecker, userController.createUser);
-// router.get('/', userController.getAllUser);
-// router.patch('/:userId', body('name').notEmpty(), validatorErrorChecker, userController.updateUser);
-// router.delete('/:userId', userController.deleteUser);
->>>>>>> main:src/router/userRouter.ts
-
-// export default router;
+export default router;
