53a9bfa6de8a46b5c2452dfe34cb4cfd060a606d
unanchoi <unusualc@likelion.org>
2022-11-25 09:23:48 +0900

    [CHORE] 장바구니 목록 조회 API JSON 형식 변경

--- src/service/basketService.ts
+++ src/service/basketService.ts
@@ -1,13 +1,12 @@
-import { PrismaClient, basket } from '@prisma/client';
-import { basket } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const addBasket = async (userId:number,bookId:number) => {
+const addBasket = async (userId:number, bookId2:number) => {
     try {
         const checkBasket = await prisma.basket.findFirst({
             where:{
-                book_id: bookId
+                book_id: bookId2
             }
         })
         console.log(checkBasket)
@@ -17,7 +16,7 @@ const addBasket = async (userId:number,bookId:number) => {
         const data = await prisma.basket.create({
             data: {
                 user_id: userId,
-                book_id: bookId
+                book_id: bookId2
             }
         });
         return data;
@@ -28,28 +27,60 @@ const addBasket = async (userId:number,bookId:number) => {
     }
 };
 
+
 const getBasketList = async (userId:number) => {
-    const basketData = await prisma.basket.findMany({
-        where: {
-            user_id: userId
-        },
-        include: {
-            book: {
-                select:{
-                    id: true,
-                    name: true,
-                    cover: true,
-                    painter: true,
-                    price: true,
-                    discount_rate: true,
-                    point: true
-                }
+    // const basketData = await prisma.basket.findMany({
+    //     where: {
+    //         user_id: userId
+    //     },
+    //     include: {
+    //         book: {
+    //             select:{
+    //                 id: true,
+    //                 name: true,
+    //                 cover: true,
+    //                 painter: true,
+    //                 price: true,
+    //                 discount_rate: true,
+    //                 point: true
+    //             }
+    //         }
+    //     }
+    // })
+    try {
+        const basketIdList = await prisma.basket.findMany({
+            where: {
+                user_id: userId
+            },
+            select: {
+                id: true
             }
+        })
+        const dataList:object[] = [];
+        for (let i =0; i < basketIdList.length; i++) {
+            const newId = basketIdList[i].id;
+            console.log(newId);
+            if ( newId == undefined) {
+                return 400;
+            }
+
+            const bookData = await prisma.book.findFirst({
+                where: {
+                    id: newId
+            }
+        })
+        console.log(bookData);
+         dataList.push(bookData);
+         console.log(dataList);
         }
-    })
-    return basketData;
     
-    // const returnDataList = []
+        return dataList;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+    
+
     // for (let i=0; basketData.length; i++) {
         
     // }
