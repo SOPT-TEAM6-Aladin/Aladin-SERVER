c4897d2609c43984cc8581b990edc2e32dd6ba9d
namjiyun <namjiyun3275@gmail.com>
2022-11-25 12:52:52 +0900

    [FIX] add basket

--- src/service/basketService.ts
+++ src/service/basketService.ts
@@ -4,74 +4,59 @@ import { basket } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const addBasket = async (userId:number,bookId:number) => {
-    try {
-        const checkBasket = await prisma.basket.findFirst({
-            where:{
-                book_id: bookId
-            }
-        })
-        console.log(checkBasket)
-        if ( checkBasket == null ) {
-            const data = await prisma.basket.create({
-                data: {
-                    user_id: userId,
-                    book_id: bookId
-                }
-            });
-            return data;
-        }
-        return 400;
-
-
-    } catch (error) {
-        console.log(error)
-        throw error
-    }
-    const data = await prisma.basket.create({
-      data: {
+const addBasket = async (userId: number, bookId: number) => {
+  try {
+    const checkBasket = await prisma.basket.findFirst({
+      where: {
+        book_id: bookId,
         user_id: userId,
-        book_id: bookId2,
       },
     });
-    return data;
+    console.log(checkBasket);
+    if (checkBasket == null) {
+      const data = await prisma.basket.create({
+        data: {
+          user_id: userId,
+          book_id: bookId,
+        },
+      });
+      return data;
+    }
+    return;
   } catch (error) {
     console.log(error);
     throw error;
   }
 };
 
-const getBasketList = async (userId:number) => {
-    const data = await prisma.basket.findMany({
-        where: {
-          id: newId,
-        },
+const getBasketList = async (userId: number) => {
+  const data = await prisma.basket.findMany({
+    where: {
+      id: newId,
+    },
+    select: {
+      book: {
         select: {
-            book: {
-                select:
-                    {
-                        id:true,
-                        name:true,
-                        cover:true,
-                        painter:true,
-                        price:true,
-                        discount_rate:true,
-                        point:true
-                    }
-            }
-        }
-    });
-    // let dataList:any = {};
-    // let returnDataList = []
-    // for (let i=0; i < data.length; i++) {
-    //     dataList["book"] = data[i].book;
-    // }
-    // return dataList
-    return data;
-    };
-
-    const basketService = {addBasket,
-  getBasketList,
+          id: true,
+          name: true,
+          cover: true,
+          painter: true,
+          price: true,
+          discount_rate: true,
+          point: true,
+        },
+      },
+    },
+  });
+  // let dataList:any = {};
+  // let returnDataList = []
+  // for (let i=0; i < data.length; i++) {
+  //     dataList["book"] = data[i].book;
+  // }
+  // return dataList
+  return data;
 };
+
+const basketService = { addBasket, getBasketList };
 
 export default basketService;
